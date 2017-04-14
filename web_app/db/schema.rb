e5e1b2746d32f69e5257ee8788d49ca4b74bf81c
bamf0351 <sun.204@osu.edu>
2017-04-14 19:15:48 -0400

    fixed routing for students and new recommendations

--- web_app/db/schema.rb
+++ web_app/db/schema.rb
@@ -103,7 +103,7 @@ ActiveRecord::Schema.define(version: 20170414173228) do
     t.string   "fname"
     t.boolean  "admin",                  default: false
     t.integer  "role"
-    t.boolean  "student",                default: false
+    t.boolean  "student"
     t.string   "lname"
     t.boolean  "professor",              default: false
     t.boolean  "stud"
