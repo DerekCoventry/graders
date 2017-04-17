13321f1ac439189374a505d1fcf15be7c065e143
derekcoventry <coventrydc@gmail.com>
2017-04-17 17:39:36 -0400

    fix changes

--- web_app/app/test.rb
+++ web_app/app/test.rb
@@ -3,17 +3,11 @@ require 'json'
 file = File.open("Su 2017 Course Schedule.json", "r")
 lines = IO.readlines(file) 
 a_hash = JSON.parse(lines[0].to_s)
-puts a_hash.keys
+puts a_hash
 file.close
 
-
-
-require 'json'
-	
-	def initialize
-		file = File.open("../helpers/Su 2017 Course Schedule.json", "r")
-		lines = IO.readlines(file) 
-		new_record = Model.new(JSON.parse(lines[0].to_s)) 
-		new_record.save
-		file.close
-	end
+# file = File.open("../helpers/Su 2017 Course Schedule.json", "r")
+# lines = IO.readlines(file) 
+# new_record = Model.new(JSON.parse(lines[0].to_s)) 
+# new_record.save
+# file.close
