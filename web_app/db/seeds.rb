983f0ea4ee74a1b0c955eeac4c772c29deba85a5
Vyyom-Kelkar <kelkar.20@osu.edu>
2017-04-18 01:07:13 -0400

    added scraper to db folder, modified seeds.rb, rails db:seed now populates the database as expected.

--- web_app/db/seeds.rb
+++ web_app/db/seeds.rb
@@ -1,3 +1,37 @@
+require 'json'
+load './db/scrape_courses.rb'
+
+file = File.open("Su 2017 Course Schedule.json", "r")
+lines = IO.readlines(file) 
+file.close
+a_hash = JSON.parse(lines[0].to_s)
+iter = 0
+a_hash.each do |x|
+	if x[1].length > iter then
+		iter = x[1].length
+	end
+end
+a_arr = a_hash.to_a
+for i in 0..iter - 1 do
+	new_record = Course.create!(courseNumber: ((a_arr[0])[1])[i],
+								mondayStart: ((a_arr[1])[1])[i],
+								mondayEnd: ((a_arr[2])[1])[i],
+								tuesdayStart: ((a_arr[3])[1])[i],
+								tuesdayEnd: ((a_arr[4])[1])[i],
+								wednesdayStart: ((a_arr[5])[1])[i],
+								wednesdayEnd: ((a_arr[6])[1])[i],
+								thursdayStart: ((a_arr[7])[1])[i],
+								thursdayEnd: ((a_arr[8])[1])[i],
+								fridayStart: ((a_arr[9])[1])[i],
+								fridayEnd: ((a_arr[10])[1])[i],
+								professor: ((a_arr[11])[1])[i],
+								sectionNumber: ((a_arr[12])[1])[i],
+								)
+end
+
+
+
+
 # # This file should contain all the record creation needed to seed the database with i: ues.
 # # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 # #
